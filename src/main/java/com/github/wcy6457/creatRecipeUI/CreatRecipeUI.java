ffd065d3c9a13eb107aaf099ac11289e8e454435
WuChenyu <645748700@qq.com>
2025-08-05 21:40:00 +0800

    creat the project

---
+++ src/main/java/com/github/wcy6457/creatRecipeUI/CreatRecipeUI.java
@@ -0,0 +1,17 @@
+package com.github.wcy6457.creatRecipeUI;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CreatRecipeUI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
