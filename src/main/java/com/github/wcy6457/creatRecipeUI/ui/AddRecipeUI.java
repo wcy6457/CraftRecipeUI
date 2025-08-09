e1106673b0b45b2aedc7ba81d1df4762d296efbc
WuChenyu <645748700@qq.com>
2025-08-09 10:52:31 +0800

    实现页面跳转。

--- src/main/java/com/github/wcy6457/creatRecipeUI/ui/AddRecipeUI.java
+++ src/main/java/com/github/wcy6457/creatRecipeUI/ui/AddRecipeUI.java
@@ -1,26 +1,24 @@
 package com.github.wcy6457.creatRecipeUI.ui;
 
 import com.github.wcy6457.creatRecipeUI.CreatRecipeUI;
-import me.devnatan.inventoryframework.*;
+import me.devnatan.inventoryframework.View;
+import me.devnatan.inventoryframework.ViewConfigBuilder;
+import me.devnatan.inventoryframework.ViewType;
 import me.devnatan.inventoryframework.context.CloseContext;
 import me.devnatan.inventoryframework.context.RenderContext;
 import me.devnatan.inventoryframework.context.SlotClickContext;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.Material;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
-public final class Menu extends View {
-
+public class AddRecipeUI extends View {
     private final CreatRecipeUI plugin;
 
-    public Menu(CreatRecipeUI plugin) {
+    public AddRecipeUI (CreatRecipeUI plugin) {
         this.plugin = plugin;
     }
 
     @Override
     public void onInit(@NotNull ViewConfigBuilder config) {
-        config.title(plugin.languageManager.get("gui.title_menu"));
+        config.title(plugin.languageManager.get("gui.title_addRecipeUI"));
         config.size(3);
         config.type(ViewType.CHEST);
         config.cancelOnClick();
@@ -29,15 +27,6 @@ public final class Menu extends View {
     @Override
     public void onFirstRender(@NotNull RenderContext render) {
 
-        ItemStack table = new ItemStack(Material.CRAFTING_TABLE);
-        ItemMeta itemMeta = table.getItemMeta();
-        itemMeta.setDisplayName(plugin.languageManager.get("gui.icon_addRecipe"));
-        table.setItemMeta(itemMeta);
-
-        render.slot(0, table)
-                .onClick(click -> {
-                    click.openForPlayer(AddRecipeUI.class);
-        });
     }
 
     @Override
